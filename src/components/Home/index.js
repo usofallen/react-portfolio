6265e452c65413196578700bd0d2b3c2ee869f09
usofallen <dylanbarrattallen@gmail.com>
2022-07-27 16:24:48 -0600

    progress!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,59 @@
-import LogoTitle from "../../assets/images/logo-s.png";
+import LogoA from "../../assets/images/logo-aa.png";
 import { Link } from "react-router-dom";
 import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray1 = [" ", "D", "y", "l", "a", "n"];
+  const nameArray2 = ["l", "l", "e", "n", ","];
+  const jobArray = [
+    "p",
+    "r",
+    "o",
+    "d",
+    "u",
+    "c",
+    "t",
+    " ",
+    "m",
+    "a",
+    "n",
+    "a",
+    "g",
+    "e",
+    "r",
+  ];
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          ylan,
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          product manager
+          <span className={`${letterClass} _12`}>I'</span>
+          <span className={`${letterClass} _12`}>m</span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray1}
+            idx={15}
+          />
+          <img src={LogoA} alt="Allen" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray2}
+            idx={17}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
-        <h2> Frontend Developer / Business Strategy</h2>
+        <h2> Frontend Developer / Business Strategist / Aspiring APM Intern</h2>
         <Link to="/contact" className="flat-button">
           CONTACT ME
         </Link>
